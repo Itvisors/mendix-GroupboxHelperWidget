fb992252a69f5aa731a10bd294073a0f26fe98e8
marcel <mgroeneweg@synobsys.nl>
2014-12-18 09:12:28 +0100

    AMD version of the widget

--- src/GroupboxHelperWidget/widget/GroupboxHelperWidget.js
+++ src/GroupboxHelperWidget/widget/GroupboxHelperWidget.js
@@ -14,70 +14,76 @@
 	Groupbox helper. This widget finds its previous sibling in the DOM tree and moves it to the header of the groupbox.
 	
 */
-dojo.provide("GroupboxHelperWidget.widget.GroupboxHelperWidget");
+(function () {
+    'use strict';
 
-dojo.declare("GroupboxHelperWidget.widget.GroupboxHelperWidget", [ mxui.widget._WidgetBase ], {
-    postCreate : function () {
-        'use strict';
+    require([
 
-        var thisObj = this;
+        'dojo/_base/declare', 'mxui/widget/_WidgetBase', 'dijit/_Widget', 'dojo/dom-construct'
 
-        setTimeout(function () {
-//            console.log('GroupboxHelperWidget: moveSiblingElement');
-            var
-                headerContainerTable,
-                headerContainerTableRow,
-                headerContainerLeftTd,
-                headerContainerRightTd,
-                headerNode,
-                headerIconNode,
-                node = thisObj.domNode.parentNode,
-                nodeToMove = thisObj.domNode.previousSibling;
+    ], function (declare, _WidgetBase, _Widget, domConstruct) {
 
-            if (nodeToMove) {
-                if (node.classList.contains("mx-groupbox-body")) {
-                    if (node.previousSibling) {
-                        headerNode = node.previousSibling;
-                        if (headerNode.classList.contains("mx-groupbox-header")) {
-                            if (thisObj.attributeValue && thisObj.attributeValue !== "") {
-                                nodeToMove.setAttribute('cssSelectorHelper', thisObj.attributeValue);
+        // Declare widget.
+        return declare('GroupboxHelperWidget.widget.GroupboxHelperWidget', [ _WidgetBase, _Widget ], {
+            postCreate : function () {
+
+                var thisObj = this;
+
+                setTimeout(function () {
+        //            console.log('GroupboxHelperWidget: moveSiblingElement');
+                    var
+                        headerContainerTable,
+                        headerContainerTableRow,
+                        headerContainerLeftTd,
+                        headerContainerRightTd,
+                        headerNode,
+                        headerIconNode,
+                        node = thisObj.domNode.parentNode,
+                        nodeToMove = thisObj.domNode.previousSibling;
+
+                    if (nodeToMove) {
+                        if (node.classList.contains("mx-groupbox-body")) {
+                            if (node.previousSibling) {
+                                headerNode = node.previousSibling;
+                                if (headerNode.classList.contains("mx-groupbox-header")) {
+                                    if (thisObj.attributeValue && thisObj.attributeValue !== "") {
+                                        nodeToMove.setAttribute('cssSelectorHelper', thisObj.attributeValue);
+                                    }
+                                    headerIconNode = headerNode.firstChild;
+                                    headerContainerTable = document.createElement("table");
+                                    headerContainerTable.style.width = "100%";
+                                    headerContainerTableRow = document.createElement("tr");
+                                    headerContainerTable.appendChild(headerContainerTableRow);
+                                    headerContainerLeftTd = document.createElement("td");
+                                    headerContainerTableRow.appendChild(headerContainerLeftTd);
+                                    headerContainerRightTd = document.createElement("td");
+                                    headerContainerTableRow.appendChild(headerContainerRightTd);
+                                    headerContainerLeftTd.appendChild(nodeToMove);
+                                    domConstruct.place(headerContainerTable, headerNode, "only");
+                                    headerContainerRightTd.appendChild(headerIconNode);
+                                    if (thisObj.attributeValue && thisObj.attributeValue !== "") {
+                                        nodeToMove.setAttribute('cssSelectorHelper', thisObj.attributeValue);
+                                        headerContainerTable.setAttribute('cssSelectorHelper', thisObj.attributeValue + "Table");
+                                    }
+                                } else {
+                                    console.warn('GroupboxHelperWidget: Groupbox header not found');
+                                }
+                            } else {
+                                console.warn('GroupboxHelperWidget: No previous sibling found for groupbox body');
                             }
-                            headerIconNode = headerNode.firstChild;
-                            headerContainerTable = document.createElement("table");
-                            headerContainerTable.style.width = "100%";
-                            headerContainerTableRow = document.createElement("tr");
-                            headerContainerTable.appendChild(headerContainerTableRow);
-                            headerContainerLeftTd = document.createElement("td");
-                            headerContainerTableRow.appendChild(headerContainerLeftTd);
-                            headerContainerRightTd = document.createElement("td");
-                            headerContainerTableRow.appendChild(headerContainerRightTd);
-                            headerContainerLeftTd.appendChild(nodeToMove);
-                            dojo.place(headerContainerTable, headerNode, "only");
-                            headerContainerRightTd.appendChild(headerIconNode);
-                            if (thisObj.attributeValue && thisObj.attributeValue !== "") {
-                                nodeToMove.setAttribute('cssSelectorHelper', thisObj.attributeValue);
-                                headerContainerTable.setAttribute('cssSelectorHelper', thisObj.attributeValue + "Table");
-                            }
+
                         } else {
-                            console.warn('GroupboxHelperWidget: Groupbox header not found');
+                            console.warn('GroupboxHelperWidget: Parent is not a groupbox');
                         }
+
                     } else {
-                        console.warn('GroupboxHelperWidget: No previous sibling found for groupbox body');
+                        console.warn('GroupboxHelperWidget: No previous sibling found');
                     }
 
-                } else {
-                    console.warn('GroupboxHelperWidget: Parent is not a groupbox');
-                }
+                }, 100);
 
-            } else {
-                console.warn('GroupboxHelperWidget: No previous sibling found');
             }
+        });
+    });
 
-        }, 100);
-
-    },
-    uninitialize : function () {
-        'use strict';
-//        console.log('GroupboxHelperWidget: uninitialize');
-    }
-});
+}());
